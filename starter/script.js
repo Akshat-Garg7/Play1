d69faa23b0512ac33dc4c3b1420ece514c422de6
Akshat-Garg7 <akshatgarg280503@gmail.com>
2025-01-11 22:42:00 +0530

    Remove Redundancy

--- starter/script.js
+++ starter/script.js
@@ -1,21 +1,48 @@
 'use strict';
 
 const dice=document.querySelector('.dice');
-const player1score=document.getElementById('score--0');
-const player2score=document.getElementById('score--1');
 const rollbtn=document.querySelector('.btn--roll');
 const holdbtn=document.querySelector('.btn--hold');
 const newbtn=document.querySelector('.btn--new');
+
+const player1score=document.getElementById('score--0');
+const player2score=document.getElementById('score--1');
 const currentplayer1=document.getElementById('current--0');
 const currentplayer2=document.getElementById('current--1');
 const background1=document.querySelector('.player--0');
 const background2=document.querySelector('.player--1');
 
-dice.classList.add('hidden');
-player1score.textContent=0;
-player2score.textContent=0;
-let lessthan=true;
-let currentscore=0;
+let currentscore,lessthan,activeplayer;
+
+const init=function()
+{
+    currentscore=0;
+    lessthan=true;
+    activeplayer=0;
+
+    player1score.textContent=0;
+    player2score.textContent=0;
+    currentplayer1.textContent=0;
+    currentplayer2.textContent=0;
+    
+    background1.classList.add('player--active');
+    background1.classList.remove('player--winner');
+    background2.classList.remove('player--active');
+    background2.classList.remove('player--winner');
+    
+    dice.classList.add('hidden');
+}
+init();
+
+const switchPlayer=function()
+{
+    document.getElementById(`current--${activeplayer}`).textContent=0;
+    currentscore=0;
+    activeplayer=activeplayer===0?1:0;
+    background1.classList.toggle('player--active');
+    background2.classList.toggle('player--active');
+}
+
 rollbtn.addEventListener('click',function()
 {
     if(lessthan)
@@ -26,17 +53,11 @@ rollbtn.addEventListener('click',function()
     if(randomnum!==1)
     {
         currentscore+=randomnum;
-        if(background1.classList.contains('player--active')) currentplayer1.textContent=currentscore;
-        else currentplayer2.textContent=currentscore;
+        document.getElementById(`current--${activeplayer}`).textContent=currentscore;
     }
     else
     {
-        currentscore=0;
-        if(background1.classList.contains('player--active')) currentplayer1.textContent=currentscore;
-        else currentplayer2.textContent=currentscore;
-        background1.classList.toggle('player--active');
-        background2.classList.toggle('player--active');
-
+        switchPlayer();
     }
 }
 }
@@ -46,57 +67,54 @@ holdbtn.addEventListener('click',function()
 {
     if(lessthan)
     {
-    if(background1.classList.contains('player--active'))  
-    {
-        player1score.textContent=Number(player1score.textContent)+Number(currentplayer1.textContent);
-        currentscore=0;
-        currentplayer1.textContent=currentscore;
-        if(Number(player1score.textContent)>=20)
+        document.getElementById(`score--${activeplayer}`).textContent=Number(document.getElementById(`score--${activeplayer}`).textContent)+Number(document.getElementById(`current--${activeplayer}`).textContent);
+        if(Number(document.getElementById(`score--${activeplayer}`).textContent)>=20)
         {
-            background1.classList.add('player--winner');
+            document.querySelector(`.player--${activeplayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activeplayer}`).classList.remove('player--active');
             lessthan=false;
             dice.classList.add('hidden');
+            
         }
-        else{
-        background1.classList.toggle('player--active');
-        background2.classList.toggle('player--active');
-        }
+        else switchPlayer();
+
+
+    // if(background1.classList.contains('player--active'))  
+    // {
+    //     player1score.textContent=Number(player1score.textContent)+Number(currentplayer1.textContent);
+    //     currentscore=0;
+    //     currentplayer1.textContent=currentscore;
+    //     if(Number(player1score.textContent)>=20)
+    //     {
+    //         background1.classList.add('player--winner');
+    //         lessthan=false;
+    //         dice.classList.add('hidden');
+    //     }
+    //     else{
+    //     background1.classList.toggle('player--active');
+    //     background2.classList.toggle('player--active');
+    //     }
         
-    }
-    else 
-    {
-        player2score.textContent=Number(player2score.textContent)+Number(currentplayer2.textContent);
-        currentscore=0;
-        currentplayer2.textContent=currentscore;
+    // }
+    // else 
+    // {
+    //     player2score.textContent=Number(player2score.textContent)+Number(currentplayer2.textContent);
+    //     currentscore=0;
+    //     currentplayer2.textContent=currentscore;
         
-        if(Number(player2score.textContent)>=20)
-        {
-            background2.classList.add('player--winner');
-            lessthan=false;
-            dice.classList.add('hidden');
-        }
-        else{
-            background1.classList.toggle('player--active');
-            background2.classList.toggle('player--active');
-        }
-    }
+    //     if(Number(player2score.textContent)>=20)
+    //     {
+    //         background2.classList.add('player--winner');
+    //         lessthan=false;
+    //         dice.classList.add('hidden');
+    //     }
+    //     else{
+    //         background1.classList.toggle('player--active');
+    //         background2.classList.toggle('player--active');
+    //     }
+    // }
 }
 }
 );
 
-newbtn.addEventListener('click',function()
-{
-
-    player1score.textContent=0;
-    player2score.textContent=0;
-    currentplayer1.textContent=0;
-    currentplayer2.textContent=0;
-    currentscore=0;
-    background1.classList.add('player--active');
-    background1.classList.remove('player--winner');
-    background2.classList.remove('player--active');
-    background2.classList.remove('player--winner');
-    lessthan=true;
-    dice.classList.add('hidden');
-}
-);
+newbtn.addEventListener('click',init);
